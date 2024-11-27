d41c378d9551a0c1fa9db2b2ba016a5dc9af7728
Tia Marie Gordon <tmgordon8192@gmail.com>
2024-11-27 15:04:30 -0800

    Installed ejs and express. Created app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+// Get the express package 
+const e = require('express');
+const express = require('express');
+
+// Instantiate an express (web) app
+const app = express();
+
+// Define a port number for the app to listen on
+const PORT = 3000;
+
+// Tell the app to encode data into JSON format
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+// Set your view (templating) engine to "EJS"
+// (We use a templating engine to create dynamic web pages)
+app.set('view engine', 'ejs');
+
+// Define a "default" route, 
+// e.g. jshmo.greenriverdev.com/reservation-app/
+app.get('/', (req, res) => {
+
+    // Return home page
+    res.render('home');
+});
+
+// Define a "confirm" route, using the POST method
+app.post('/confirm', (req, res) => {
+    // Get the data from the form that was submitted
+    // from the body of the request object
+    let details = req.body;
+
+    // Display the confirm page, pass the data
+    res.render('confirm', { details: details });
+})
+
+// Tell the app to listen for requests on the designated port
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`)
+});
