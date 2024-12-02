8e2f45f0a2a6d60a917cb96cd14a2b2f47d2d8a1
Tav Singh <147795944+TavSingh@users.noreply.github.com>
2024-12-02 22:45:08 +0000

    Edited app.js to access the database, and editing the Alchemist page to show that it is being worked on.

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ const app = express();
 // Define a port number for the app to listen on
 const PORT = 3000;
 
+// Give information to connect to mariadb
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'fli_guide'
+});
+
+// Function that connects to mariadb
+async function connect() {
+    try {
+        let conn = await pool.getConnection();
+        console.log('Connected to the database');
+        // Return connection so that it can be used
+        return conn;
+    } catch (err) {
+        console.log ('Error connecting to the database: ' + err)
+    }
+}
+
 // Tell the app to encode data into JSON format
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -32,59 +52,59 @@ app.get('/new-home', (req, res) => {
 
 app.get('/alchemist-guide', (req, res) => {
     res.render('life_guides/alchemist-guide');
-})
+});
 
 app.get('/angler-guide', (req, res) => {
     res.render('life_guides/angler-guide');
-})
+});
 
 app.get('/artist-guide', (req, res) => {
     res.render('life_guides/artist-guide');
-})
+});
 
 app.get('/blacksmith-guide', (req, res) => {
     res.render('life_guides/blacksmith-guide');
-})
+});
 
 app.get('/carpenter-guide', (req, res) => {
     res.render('life_guides/carpenter-guide');
-})
+});
 
 app.get('/cook-guide', (req, res) => {
     res.render('life_guides/cook-guide');
-})
+});
 
 app.get('/farmer-guide', (req, res) => {
     res.render('life_guides/farmer-guide');
-})
+});
 
 app.get('/hunter-guide', (req, res) => {
     res.render('life_guides/hunter-guide');
-})
+});
 
 app.get('/magician-guide', (req, res) => {
     res.render('life_guides/magician-guide');
-})
+});
 
 app.get('/mercenary-guide', (req, res) => {
     res.render('life_guides/mercenary-guide');
-})
+});
 
 app.get('/miner-guide', (req, res) => {
     res.render('life_guides/miner-guide');
-})
+});
 
 app.get('/paladin-guide', (req, res) => {
     res.render('life_guides/paladin-guide');
-})
+});
 
 app.get('/tailor-guide', (req, res) => {
     res.render('life_guides/tailor-guide');
-})
+});
 
 app.get('/woodcutter-guide', (req, res) => {
     res.render('life_guides/woodcutter-guide');
-})
+});
 
 // Define a "confirm" route, using the POST method
 app.post('/confirm', (req, res) => {
@@ -94,7 +114,7 @@ app.post('/confirm', (req, res) => {
 
     // Display the confirm page, pass the data
     res.render('confirm', { details: details });
-})
+});
 
 // Tell the app to listen for requests on the designated port
 app.listen(PORT, () => {
