8091cec4d487b3a6ef74ab0cc53462267787516e
Tav Singh <147795944+TavSingh@users.noreply.github.com>
2024-12-02 23:02:48 +0000

    Fixed the script.

--- sql_script/database.sql
+++ sql_script/database.sql
@@ -3,6 +3,9 @@ CREATE DATABASE fli_guide;
 CREATE TABLE crafting (
     id INT AUTO_INCREMENT PRIMARY KEY,
     main_resource VARCHAR(255),
+    main_num INT,
     sec_resource VARCHAR(255),
+    sec_num INT,
     opt_reagent VARCHAR(255)
+    opt_num INT
 );
