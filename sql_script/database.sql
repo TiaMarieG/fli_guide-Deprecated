ba145af7943fbd93515b6f0ef3588a8e14680d27
Tia Marie Gordon <tmgordon8192@gmail.com>
2024-12-02 19:22:07 -0800

    Created craft selection in BS skill. Added data to bs_crafting table

--- sql_script/database.sql
+++ sql_script/database.sql
@@ -10,3 +10,30 @@ CREATE TABLE alchemy_crafting (
     opt_reagent VARCHAR(255),
     opt_num INT
 );
+
+CREATE TABLE bs_crafting (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    craft_name VARCHAR(255),
+    main_resource VARCHAR(255),
+    main_num int,
+    sec_resource VARCHAR(255),
+    sec_num int,
+    opt_reagent VARCHAR(255),
+    opt_num int
+);
+
+INSERT INTO bs_crafting (craft_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("iron sword", "iron ingot", 5, "leather", 2, "reflux", 1);
+
+SELECT * FROM bs_crafting;
+
+INSERT INTO bs_crafting (craft_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("iron gloves", "iron ingot", 2, "leather", 4, "reflux", 1);
+
+INSERT INTO bs_crafting (craft_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("iron helmet", "iron ingot", 4, "leather", 3, "reflux", 2);
+
+INSERT INTO bs_crafting (craft_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("iron pants", "iron ingot", 8, "leather", 5, "reflux", 3);
+
+SELECT * FROM bs_crafting;
