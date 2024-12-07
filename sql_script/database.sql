7dfcf5e3f7a0d92092a0d857cfbdcc611efa6f1e
Tavparsad Singh <tavparsadsingh2@gmail.com>
2024-12-07 14:20:08 -0800

    Added changes to pages and sql.

--- sql_script/database.sql
+++ sql_script/database.sql
@@ -49,7 +49,18 @@ VALUES ("cooking", "cheesecake", "cheese", 3, "cake", 3, "strawberries", 3);
 INSERT INTO crafting_recipes (craft_skill, item_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
 VALUES ("cooking", "cheeseburger", "cheese", 1, "hamburger", 2, "lettuce", 1);
 
+
 INSERT INTO crafting_recipes (craft_skill, item_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
 VALUES ("alchemy", "hp potion", "healweed", 1, "springwater", 1);
+
+INSERT INTO crafting_recipes (craft_skill, item_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("alchemy", "sp potion", "vitalweed", 2, "springwater", 1);
+
+INSERT INTO crafting_recipes (craft_skill, item_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("alchemy", "poison antidote", "cureweed", 1, "antidote berries", 1, "springwater", 1);
+
+INSERT INTO crafting_recipes (craft_skill, item_name, main_resource, main_num, sec_resource, sec_num, opt_reagent, opt_num)
+VALUES ("alchemy", "stun antidote", "cureweed", 1, "antistun berries", 1, "springwater", 1);
+
 
 SELECT * FROM crafting_recipes;
